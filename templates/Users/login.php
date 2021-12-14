96b09e97567e6e482bbc38997102915d4f199201
Bruno Vicente <brunnovicente@gmail.com>
2021-12-14 12:18:21 -0300

    Login
    Nova tela de login responsiva.

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -4,6 +4,39 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 mx-auto shadow p-3 mt-3 rounded">
+            <?= $this->Form->create()?>
+            <div class="container mb-4">
+                <a href="/"><img src="/img/batman.png" class="d-block mx-auto w-25"/></a>
+            </div>
+            <div class="logo mb-3">
+                <div class="col-md-12 text-center">
+                    <h3>Camara dos Vereadores<br>Coelho Neto - MA</h3>
+                </div>
+            </div>
+            <div class="form-group">
+                <?php echo $this->Form->control('username', ['class'=>'form-control', 'label'=>'LOGIN']);?>
+            </div>
+            <div class="form-group">
+                <?php echo $this->Form->control('password', ['class'=>'form-control', 'label'=>'SENHA']);?>
+            </div>
+            <div>
+                <?= $this->Form->button('Entrar', ['class'=>'btn btn-success btn-block'])?>
+            </div>
+            <?= $this->Form->end()?>
+            <div class="row">
+                <div class="col">
+                    <a class="btn btn-light btn-block mt-2" href="/">Voltar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--
 <br>
 <div class="container shadow col-4 p-2">
     <div class="row mb-5 justify-content-center">
@@ -46,3 +79,4 @@
         </div>
     </div>
 </div>
+-->
